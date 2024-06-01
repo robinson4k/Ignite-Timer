3ea14289acf1d3d4f771a80e963607fc163cb23a
Robinson <robinson4k@gmail.com>
2024-06-01 16:48:14 -0400

    Header & Layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
 
       {/* é um espaco para ser inserido com conteudo */}
-      Conteudo
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
