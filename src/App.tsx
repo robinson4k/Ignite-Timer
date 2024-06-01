511525a2cf0f2d276f720c1b200b9e204a18e23c
Robinson <robinson4k@gmail.com>
2024-06-01 13:17:31 -0400

    Estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>teste</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
